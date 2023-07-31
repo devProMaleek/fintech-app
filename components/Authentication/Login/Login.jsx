53426f9b782504b7989c8670831047030b4e3c5b
Abdulmalik Adebayo <abdulmalikadebayo07@gmail.com>
2023-07-31 22:51:25 +0100

    refactor:  Refactor this component to clear lint error

--- components/Authentication/Login/Login.jsx
+++ components/Authentication/Login/Login.jsx
@@ -42,7 +42,7 @@ const Login = (props) => {
     if (isSubmitSuccessful && !isSubmitting) {
       reset();
     }
-  }, [isSubmitting, isSubmitSuccessful]);
+  }, [isSubmitting, isSubmitSuccessful, reset]);
 
   const isValidEmail = (email) =>
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
